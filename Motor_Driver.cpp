b30c1337bf04fcc2100483be5b23decabadd0e7f
Swordig0 <127114216+Swordig0@users.noreply.github.com>
2024-07-15 21:00:31 +0530

    MD_2.0
    *Added motor driver logic

--- Motor_Driver.cpp
+++ Motor_Driver.cpp
@@ -1,5 +1,16 @@
+//MOTOR DRIVER
+//Project INSEE Gantry Crane Automation
+//DETE-SEUSL
+//Author: MALINDA MADUMAL
+//2024.04.04
+
 #include <ArduinoJson.h>
 #include <AFMotor.h>
+
+AF_DCMotor motor_drive(1);
+
+const char* driveM_dirct;
+int driveM_speed = 0;
 
 void receiveJson() {
   // Check if data is available to read from the ESP32
@@ -11,18 +22,32 @@ void receiveJson() {
 
     if (!error) {
       //Extract values from json
-      const char* driveM_dirct = doc["driveM_dirct"];
-      int driveM_speed = doc["driveM_speed"];
+      driveM_dirct = doc["driveM_dirct"];
+      driveM_speed = doc["driveM_speed"];
     }
   }
 }
 
 void setup() {
   Serial.begin(9600);
+  motor_drive.run(RELEASE);
 }
 
 void loop() {
   void receiveJson();
-  delay(300);
+
+  motor_drive.setSpeed(driveM_speed);
+
+  if(driveM_dirct == 'F'){
+    motor_drive.run(FORWARD);
+  }
+  else if(driveM_dirct == 'B'){
+    motor_drive.run(BACKWARD);
+  }
+  else{
+    motor_drive.run(RELEASE);
+  }
+
+  delay(100);
   
 }
